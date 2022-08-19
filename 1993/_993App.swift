cf3ceb830d003d0853aa7dabb9df2a735050953c
Elaidzha1940 <eli080293@gmail.com>
2022-08-19 18:04:11 +0300

    eli1993

---
+++ 1993/_993App.swift
@@ -0,0 +1,17 @@
+//
+//  _993App.swift
+//  1993
+//
+//  Created by Elaidzha Shchukin on 19.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct _993App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
